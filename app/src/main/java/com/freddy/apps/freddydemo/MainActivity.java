6816019f440b153f3e5a2a59a8aeba11449dcac6
Freddy <freddy.jd2@gmail.com>
2022-09-30 22:01:27 -0400

    initial

--- app/src/main/java/com/freddy/apps/freddydemo/MainActivity.java
+++ app/src/main/java/com/freddy/apps/freddydemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.freddy.apps.freddydemo;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -16,78 +17,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvFirstTextView, tvSelectedCity;
-    Button btnFirstButton;
-    EditText etName;
-    RadioGroup rgEnableButton;
-    CheckBox cbCourse1, cbCourse2, cbCourse3;
-    Spinner spCities;
+    TextView email, password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvFirstTextView = findViewById(R.id.tvFirstTextView);
-        btnFirstButton = findViewById(R.id.btnFirstButton);
-        etName = findViewById(R.id.etName);
-        rgEnableButton = findViewById(R.id.rgEnableButton);
-        cbCourse1 = findViewById(R.id.cbCourse1);
-        cbCourse2 = findViewById(R.id.cbCourse2);
-        cbCourse3 = findViewById(R.id.cbCourse3);
-        spCities = findViewById(R.id.spCities);
-        tvSelectedCity = findViewById(R.id.tvSelectedCity);
+        loginButton = findViewById(R.id.loginButton);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
-/*
-        btnFirstButton.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this, "Our Button is clicked successfully", Toast.LENGTH_SHORT).show();
-
-            String userInput = etName.getText().toString();
-            tvFirstTextView.setText(userInput);
-            etName.setText("");
-        });
-*/
-        btnFirstButton.setOnClickListener(v -> {
-            String result = "";
-            if (cbCourse1.isChecked()) {
-                result = cbCourse1.getText().toString();
-            }
-            if (cbCourse2.isChecked()) {
-                result = result + "\n" + cbCourse2.getText().toString();
-            }
-            if (cbCourse3.isChecked()) {
-                result = result + "\n" + cbCourse3.getText().toString();
-            }
-            if (result.isEmpty()) {
-                result = "No selection made";
-            }
-            tvFirstTextView.setText(result);
+        loginButton.setOnClickListener(view -> {
+            Log.i("CLICK_EVENT_DATA: ", email.getText().toString() + " " + password.getText().toString());
         });
 
-
-        rgEnableButton.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (rgEnableButton.getCheckedRadioButtonId() == R.id.rbFirst) {
-                    btnFirstButton.setEnabled(true);
-                } else {
-                    btnFirstButton.setEnabled(false);
-                }
-            }
-        });
-
-        spCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                TextView textView = (TextView) view;
-                String selectedCityText = textView.getText().toString();
-                tvSelectedCity.setText(selectedCityText + "_" + position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 }
